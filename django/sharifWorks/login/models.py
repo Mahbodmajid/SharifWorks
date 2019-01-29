84614e98c2c61392bfe24091d24246e446aa844e
hkimia <kimiahamdieh@gmail.com>
2019-01-29 16:14:07 +0330

    models

--- django/sharifWorks/login/models.py
+++ django/sharifWorks/login/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User, AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Profile(models.Model):
+    class Meta:
+        verbose_name = _('Profile')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=10)
+    bio = models.TextField()
+    joined_date = models.DateField(auto_now_add=True, null=True, blank=True)
+
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
